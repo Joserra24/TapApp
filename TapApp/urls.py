15917a493483d71275ee57f0250218a8da69ba53
Joserra24 <josbanbot@alum.us.es>
2025-03-21 19:34:39 +0100

    Añadir notas en los pedidos

--- TapApp/urls.py
+++ TapApp/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('pagar_pedido/<int:pedido_id>/', views.pagar_pedido, name='pagar_pedido'),
     path('pedidos_cerrados/', views.lista_pedidos_cerrados, name='lista_pedidos_cerrados'),
     path('pedido_cerrado/<int:pedido_id>/', views.detalle_pedido_cerrado, name='detalle_pedido_cerrado'),
+    path('pedido/<int:pedido_id>/producto/<int:producto_pedido_id>/nota/', views.actualizar_nota_producto, name='actualizar_nota_producto'),
 
 
 
